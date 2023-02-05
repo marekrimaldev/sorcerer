1cdf6a09a07e06fecfc046c8ecbb5225e41eb57c
Rimal <marekrimal97@gmail.com>
2023-02-05 19:58:38 +0100

    Casting Animation Done

--- Assets/VRBaseSetup/OculusHands/Scripts/HandAnimatorContoller.cs
+++ Assets/VRBaseSetup/OculusHands/Scripts/HandAnimatorContoller.cs
@@ -1,21 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR;
 
-public class HandPresence : MonoBehaviour
+public class HandAnimatorContoller : MonoBehaviour
 {
-    [SerializeField] private Animator _handAnimator;
     [SerializeField] private InputActionProperty _triggerAction;
     [SerializeField] private InputActionProperty _gripAction;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         float triggerVal = _triggerAction.action.ReadValue<float>();
         float gripVal = _gripAction.action.ReadValue<float>();
 
-        _handAnimator.SetFloat("Trigger", triggerVal);
-        _handAnimator.SetFloat("Grip", gripVal);
+        _animator.SetFloat("Trigger", triggerVal);
+        _animator.SetFloat("Grip", gripVal);
     }
 }
