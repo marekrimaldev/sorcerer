edb80e375ee02d4c1c63573d4ebabc7684a32264
Rimal <marekrimal97@gmail.com>
2023-02-05 18:44:06 +0100

    Physics hands done

--- Assets/VRBaseSetup/OculusHands/Scripts/HandPresencePhysics.cs
+++ Assets/VRBaseSetup/OculusHands/Scripts/HandPresencePhysics.cs
@@ -6,11 +6,34 @@ using UnityEngine.XR;
 public class HandPresencePhysics : MonoBehaviour
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private GameObject _realHand;
+    [SerializeField] private float _showRealHandDistance = .1f;
+    
     private Rigidbody _rb;
+    private Collider[] _colliders;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _colliders = GetComponentsInChildren<Collider>();
+    }
+    public void EnableCollidersDelayed(float delay)
+    {
+        StartCoroutine(EnableCollidersInSeconds(delay));
+    }
+
+    private IEnumerator EnableCollidersInSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        EnableColliders(true);
+    }
+
+    public void EnableColliders(bool value)
+    {
+        for (int i = 0; i < _colliders.Length; i++)
+        {
+            _colliders[i].enabled = value;
+        }
     }
 
     private void FixedUpdate()
@@ -22,5 +45,6 @@ public class HandPresencePhysics : MonoBehaviour
         Vector3 rotationsInDegrees = angleInDegrees * rotations;
         _rb.angularVelocity = rotationsInDegrees * Mathf.Deg2Rad / Time.fixedDeltaTime;
 
+        _realHand.SetActive(Vector3.Distance(_target.position, transform.position) > _showRealHandDistance);
     }
 }
