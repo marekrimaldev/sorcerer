2d9b459367207210ff56be2fed851894002d5661
Rimal <marekrimal97@gmail.com>
2023-02-05 16:18:28 +0100

    Basic physic hands

---
+++ Assets/VRBaseSetup/OculusHands/Scripts/HandPresencePhysics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresencePhysics : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = (_target.position - transform.position) / Time.fixedDeltaTime;
+        
+        Quaternion rotationDiff = _target.rotation * Quaternion.Inverse(transform.rotation);
+        rotationDiff.ToAngleAxis(out float angleInDegrees, out Vector3 rotations);
+        Vector3 rotationsInDegrees = angleInDegrees * rotations;
+        _rb.angularVelocity = rotationsInDegrees * Mathf.Deg2Rad / Time.fixedDeltaTime;
+
+    }
+}
