ce9bcf791643e2a0aa8cf902c0e44bc16e235ff8
Rimal <marekrimal97@gmail.com>
2023-02-07 21:13:51 +0100

    Simple charge up projectile effect done

--- Assets/Scripts/Gauntlet.cs
+++ Assets/Scripts/Gauntlet.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Gauntlet : MonoBehaviour
 {
     [SerializeField] private Projectile _projectilePrefab;
+    [SerializeField] private GameObject _chargeUpPrefab;
     [SerializeField] private Transform _palmSpawnPosition;
     [SerializeField] private Transform _fingerSpawnPosition;
+
+    private GameObject _chargeUp;
 
     public void InstantCast()
     {
@@ -17,11 +20,14 @@ public class Gauntlet : MonoBehaviour
     public void ChargingBegin()
     {
         Debug.Log("Charging begin");
+        _chargeUp = Instantiate(_chargeUpPrefab, _palmSpawnPosition.position, _palmSpawnPosition.rotation);
+        _chargeUp.transform.SetParent(_palmSpawnPosition);
     }
 
     public void ChargingEnd()
     {
         Debug.Log("Charging end");
+        Destroy(_chargeUp);
         Instantiate(_projectilePrefab, _palmSpawnPosition.position, _palmSpawnPosition.rotation);
     }
 }
