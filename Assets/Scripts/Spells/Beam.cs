8e7f98839da92c32b1cb6a1ac2b7283b80190a9c
Rimal <marekrimal97@gmail.com>
2023-02-23 20:47:33 +0100

    New dark electric beam ready

--- Assets/Scripts/Spells/Beam.cs
+++ Assets/Scripts/Spells/Beam.cs
@@ -1,30 +1,33 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Beam : HittableSpellCast
 {
-    [SerializeField] private float _laserLength;
+    [SerializeField] private float _beamLength;
     [SerializeField] private GameObject _laserPrefab;
-    [SerializeField] private GameObject _laserSourceEffectPrefab;
-    [SerializeField] private GameObject _laserEndEffectPrefab;
+    [SerializeField] private GameObject _beamSourceEffectPrefab;
+    [SerializeField] private GameObject _beamEndEffectPrefab;
+    [SerializeField] private VisualEffect _keke;
 
     private Transform _parentTransform;
 
-    private LineRenderer[] _laserRenderers;
-    private GameObject _laserInstance;
-    private GameObject _laserSourceEffectInstance;
-    private GameObject _laserEndEffectInstance;
+    private LineRenderer[] _beamRenderers;
+    private GameObject _beamInstance;
+    private GameObject _beamSourceEffectInstance;
+    private GameObject _beamEndEffectInstance;
 
     private SphereCollider _laserEndTrigger;
 
     private void Update()
     {
-        if (_laserRenderers != null && _laserRenderers.Length > 0)
+        if (_beamRenderers != null && _beamRenderers.Length > 0)
             UpdateLaser();
     }
 
     private void OnTriggerStay(Collider other)
     {
-        _onHitEffectManager.ApplyEffects(_laserEndEffectInstance.transform.position, other.transform);
+        
+        _onHitEffectManager.ApplyEffects(_beamEndEffectInstance.transform.position, other.transform);
     }
 
     public override void Init(Spell spell, float chargePercent = 1)
@@ -32,36 +35,36 @@ public class Beam : HittableSpellCast
         base.Init(spell);
 
         _parentTransform = spell.AimIndicator.SpawnPoint;
-        _laserInstance = Instantiate(_laserPrefab, _parentTransform.position, _parentTransform.rotation, transform);
-        _laserRenderers = _laserInstance.GetComponentsInChildren<LineRenderer>();
-        _laserSourceEffectInstance = Instantiate(_laserSourceEffectPrefab, _parentTransform.position, _parentTransform.rotation, transform);
-        _laserEndEffectInstance = Instantiate(_laserEndEffectPrefab, _parentTransform.position, _parentTransform.rotation, transform);
+        _beamInstance = Instantiate(_laserPrefab, _parentTransform.position, _parentTransform.rotation, transform);
+        _beamRenderers = _beamInstance.GetComponentsInChildren<LineRenderer>();
+        _beamSourceEffectInstance = Instantiate(_beamSourceEffectPrefab, _parentTransform.position, _parentTransform.rotation, transform);
+        _beamEndEffectInstance = Instantiate(_beamEndEffectPrefab, _parentTransform.position, _parentTransform.rotation, transform);
 
-        _laserEndTrigger = _laserEndEffectInstance.AddComponent<SphereCollider>();
-        _laserEndEffectInstance.layer = LayerMask.NameToLayer("IgnoreSpell");
+        _laserEndTrigger = _beamEndEffectInstance.AddComponent<SphereCollider>();
+        _beamEndEffectInstance.layer = LayerMask.NameToLayer("IgnoreSpell");
         _laserEndTrigger.isTrigger = true;
         _laserEndTrigger.radius = 0.05f;
     }
 
     private void UpdateLaser()
     {
-        Vector3 laserEnd = _parentTransform.position + _parentTransform.forward * _laserLength;
+        Vector3 laserEnd = _parentTransform.position + _parentTransform.forward * _beamLength;
         if (Physics.Linecast(_parentTransform.position, laserEnd, out RaycastHit hitInfo, ~LayerMask.GetMask("IgnoreSpell")))
         {
             laserEnd = hitInfo.point;
 
-            _laserEndEffectInstance.SetActive(true);
-            _laserEndEffectInstance.transform.position = laserEnd - _parentTransform.forward * 0.05f;
+            _beamEndEffectInstance.SetActive(true);
+            _beamEndEffectInstance.transform.position = laserEnd - _parentTransform.forward * 0.05f;
         }
         else
         {
-            _laserEndEffectInstance.SetActive(false);
+            _beamEndEffectInstance.SetActive(false);
         }
 
-        for (int i = 0; i < _laserRenderers.Length; i++)
+        for (int i = 0; i < _beamRenderers.Length; i++)
         {
-            _laserRenderers[i].SetPosition(0, _parentTransform.position);
-            _laserRenderers[i].SetPosition(1, laserEnd);
+            _beamRenderers[i].SetPosition(0, _parentTransform.position);
+            _beamRenderers[i].SetPosition(1, laserEnd);
         }
     }
 }
