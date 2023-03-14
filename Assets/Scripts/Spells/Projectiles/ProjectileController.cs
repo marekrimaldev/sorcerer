77cf964da6911c2bbf0ebc05877a36b5f7b05b39
Rimal <marekrimal97@gmail.com>
2023-03-14 21:01:36 +0100

    Before hand acc remove

--- Assets/Scripts/Spells/Projectiles/ProjectileController.cs
+++ Assets/Scripts/Spells/Projectiles/ProjectileController.cs
@@ -10,9 +10,11 @@ public class ProjectileController : MonoBehaviour
     [SerializeField] [Range(0, 1)] private float _handling = 0.5f;      // 0 = we cant controll : 1 = full controll
     [SerializeField] private bool _applyGravity = true;
     [SerializeField] private float _homingIntensity = 1f;
-    [SerializeField] private float _homingOffset = 1f;
-    [SerializeField] private float _maxOffset = .5f;
-    [SerializeField] private float _maxAcc = .01f;
+    [SerializeField] private float _homingAccuracy = 1f;
+    [SerializeField] private float _hommingRadius = 1f;
+
+    [SerializeField] private GameObject _hommingTargetPrefab;
+    private Transform _hommingTarget;
 
     private const float AccelerationMultiplier = 2;
 
@@ -25,23 +27,26 @@ public class ProjectileController : MonoBehaviour
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
-        _rb.useGravity = _applyGravity;
-
         _target = FindObjectOfType<TargetTransform>().transform;
         _targetOrientedAim = new GameObject("Target direction transform").transform;
+
+        _hommingTarget = Instantiate(_hommingTargetPrefab).transform;
     }
 
     private void OnDestroy()
     {
         if(_targetOrientedAim != null)
             Destroy(_targetOrientedAim.gameObject);
+
+        Destroy(_hommingTarget.gameObject);
     }
 
     public void SendProjectile(float chargePercent, Projectile projectile, Transform aimTransform)
     {
         _aim = aimTransform;
         _projectile = projectile;
+        _rb = _projectile.GetComponent<Rigidbody>();
+        _rb.useGravity = _applyGravity;
 
         float forceMultiplier = Mathf.Lerp(0.3f, 1, chargePercent);
         GetComponent<Rigidbody>()?.AddForce(transform.forward * projectile.Force * forceMultiplier);
@@ -51,16 +56,16 @@ public class ProjectileController : MonoBehaviour
 
     private IEnumerator ControllProjectileCoroutine(float forceMultiplier)
     {
+        yield return new WaitForSeconds(0.05f);
+
         Vector3 lastPos = _aim.position;
-        Vector3 homingAcc = Vector3.zero;
         Vector3 handAcc = Vector3.zero;
-        Vector3 acceleration = Vector3.zero;
-
-        Vector3 homingPos = _target.position + (_aim.position - _target.position).normalized * _homingOffset;
-
         _rb.transform.LookAt(_rb.transform.position + _rb.velocity);
 
         float playerTargetDistance = Vector3.Distance(_target.position, _aim.position);
+        Vector3 startVelocity = _rb.velocity;
+        Vector3 startDir = _rb.velocity.normalized;
+        _hommingTarget.position = _target.position;
 
         while (this)
         {
@@ -68,40 +73,26 @@ public class ProjectileController : MonoBehaviour
 
             _targetOrientedAim.position = _aim.position;
 
+            float distanceToTarget = (_rb.position - _target.position).magnitude;
+            Vector3 hommingPosition = distanceToTarget < _hommingRadius ? _target.position : _hommingTarget.position;
+
             // Hand acceleration
-            _targetOrientedAim.LookAt(_target);
+            _targetOrientedAim.LookAt(_hommingTarget);
             Vector3 offset = _targetOrientedAim.position - lastPos;
             offset = Vector3.ProjectOnPlane(offset, _targetOrientedAim.forward);
             handAcc = offset * AccelerationMultiplier * _projectile.Force * _handling;
             lastPos = _targetOrientedAim.position;
+            _hommingTarget.position += offset * _homingAccuracy;
 
             // Homing with velocity
             Vector3 vecToPlayer = _aim.position - _rb.position;
-            Vector3 vecToTarget = _target.position - _rb.position;
+            Vector3 vecToTarget = hommingPosition - _rb.position;
             Vector3 dirToTarget = vecToTarget.normalized;
-            Vector3 velocity = _rb.velocity;
             float t = vecToPlayer.magnitude / playerTargetDistance;
-            //velocity = Vector3.Lerp(velocity, dirToTarget * velocity.magnitude, t + _homingIntensity);
 
-            Try to somehow bing handAcc to t
-
-            if (t < 1)
-            {
-                velocity = Vector3.Lerp(velocity, dirToTarget * velocity.magnitude, t * _homingIntensity);
-                _rb.velocity = velocity;
-            }
-
-            // Homing with acceleration
-            //Vector3 vecToHand = _targetOrientedAim.position - _rb.position;
-            //Vector3 vecToTarget = _target.position - _rb.position;
-            //float ratio = vecToHand.magnitude / vecToTarget.magnitude;  // the further away the bigger
-            //homingAcc = vecToTarget.normalized * _homingIntensity;
-            //homingAcc = _rb.transform.InverseTransformVector(homingAcc);
-            //Vector3.Scale(homingAcc, new Vector3(1/ratio, ratio, ratio));
-            //homingAcc = _rb.transform.TransformVector(homingAcc);
-
-            acceleration = homingAcc + handAcc;
-            _rb.AddForce(acceleration, ForceMode.Acceleration);
+            Vector3 velocity = handAcc;
+            velocity += Vector3.Lerp(startDir, dirToTarget, t * _homingIntensity) * startVelocity.magnitude;
+            _rb.velocity = velocity;
         }
     }
 }
